a68339145159474e038d0def700b756be1b17a3f
equinox-1092 <sdiaz1234di@gmail.com>
2025-09-01 15:50:03 -0500

    commit init

---
+++ app/src/main/java/pe/edu/upc/easyshop/Login.kt
@@ -0,0 +1,94 @@
+package pe.edu.upc.easyshop
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Login(){
+
+    val email = remember {
+        mutableStateOf(value = "")
+    }
+
+    var password by remember {
+        mutableStateOf(value = "")
+    }
+
+    val isVisible = remember {
+        mutableStateOf(value = false)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        OutlinedTextField(
+            value = email.value,
+            onValueChange = {
+                email.value = it
+            },
+            modifier = Modifier.fillMaxWidth().padding(8.dp),
+            leadingIcon = {
+                Icon(Icons.Default.Email, contentDescription = null )
+            }
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            modifier = Modifier.fillMaxWidth().padding(8.dp),
+            leadingIcon = {
+                Icon(Icons.Default.Lock, contentDescription = null )
+            },
+            visualTransformation =
+                if(isVisible.value){
+                    VisualTransformation.None
+                }else{
+                    PasswordVisualTransformation()
+                },
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        isVisible.value = !isVisible.value
+                    }
+                ) {
+                    Icon(
+                        if(isVisible.value) Icons.Default.Visibility
+                        else{
+                        Icons.Default.VisibilityOff},
+                        contentDescription = null
+                    )
+                }
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LoginPreview(){
+    Login()
+}
